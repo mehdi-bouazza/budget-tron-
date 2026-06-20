1e4a5486ce878b6adb7a072eeb7ca6176cad2699
Mehdi Bouazza <61715635+mehdi-bouazza@users.noreply.github.com>
2026-06-20 09:32:44 +0200

    Separate Commit: A small file to play with Pandas, Seaborn and Numpy. Even with the available agentic coding possibilities, it's still fun to map out and utilize your data. I started with my energy consumption, in the future I like to build-out a simulation for Titman

---
+++ panda-learning.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+# Importeren van de brondata ("r" achter "(" voorkomt fouten in pathfinding)
+
+df = pd.read_csv(r"C:\Users\Mehdi Bouazza\Desktop\panda-learning")
+
+print(df.columns)
+
